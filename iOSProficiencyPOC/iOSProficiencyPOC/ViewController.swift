0d8da90027a078f33ca7a93e5bf9036f70af726f
sohambhowmik <soham.bhowmik@bigbasket.com>
2019-09-05 12:32:38 +0530

    Initial Commit - Created Project.

---
+++ iOSProficiencyPOC/iOSProficiencyPOC/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iOSProficiencyPOC
+//
+//  Created by Soham Bhowmik on 05/09/19.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor.white
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
---
+++ iOSProficiencyPOC/iOSProficiencyPOCTests/iOSProficiencyPOCTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOSProficiencyPOCTests.swift
+//  iOSProficiencyPOCTests
+//
+//  Created by Soham Bhowmik on 05/09/19.
+//  Copyright © 2019 self. All rights reserved.
+//
+
+import XCTest
+@testable import iOSProficiencyPOC
+
+class iOSProficiencyPOCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
